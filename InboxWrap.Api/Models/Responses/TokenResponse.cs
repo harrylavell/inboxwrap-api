1a7bc993740fda3ac82dcf684437d55d9b2c793e
harrylavell <hallan@outlook.com.au>
2025-05-19 21:32:50 +1000

    Add Microsoft auth controller

---
+++ InboxWrap.Api/Models/Responses/TokenResponse.cs
@@ -0,0 +1,24 @@
+using System.Text.Json.Serialization;
+
+namespace InboxWrap.Models.Reponses;
+
+public class TokenResponse
+{
+    [JsonPropertyName("token_type")]
+    public string? TokenType { get; set; }
+
+    [JsonPropertyName("scope")]
+    public string? Scope { get; set; }
+
+    [JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; set; }
+
+    [JsonPropertyName("access_token")]
+    public string? AccessToken { get; set; }
+
+    [JsonPropertyName("refresh_token")]
+    public string? RefreshToken { get; set; }
+
+    [JsonPropertyName("id_token")]
+    public string? IdToken { get; set; }
+}
